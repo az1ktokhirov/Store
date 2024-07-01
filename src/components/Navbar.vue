a8510a7f58b2daf3d1f5a282fafd3abfa5cf10af
az1ktokhirov <azizillotokhirov@gmail.com>
2024-07-01 18:51:45 +0500

    azt-store

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -40,54 +40,54 @@ const catalogIcon = computed(() => (isModalOpen.value ? "xmark" : "bars"));
                 </div>
             </div>
         </div>
+        <div :class="['nav__modal', { 'nav__modal-open': isModalOpen }]">
+            <ul class="nav__modal-list">
+                <li>
+                    <router-link to="/avtomobillar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="car" />Avtomobillar </router-link>
+                </li>
+                <li>
+                    <router-link to="/aksesuarlar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="headphones" />Aksessuarlar </router-link>
+                </li>
+                <li>
+                    <router-link to="/planshetlar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="tablet" />Planshetlar</router-link>
+                </li>
+                <li>
+                    <router-link to="/maishiy-kimyo-vositalari" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="pump-soap" />Teri parvarishi</router-link>
+                </li>
+                <li>
+                    <router-link to="/mototransportlar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="motorcycle" />Mototransportlar</router-link>
+                </li>
+                <li>
+                    <router-link to="/ko`zoynaklar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="glasses" />Ko'zoynaklar</router-link>
+                </li>
+                <li>
+                    <router-link to="/noutbuklar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="laptop" />Noutbuklar</router-link>
+                </li>
+                <li>
+                    <router-link to="/uy-dekaratsiyalari" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="chair" />Uy dekoratsiyalari</router-link>
+                </li>
+                <li>
+                    <router-link to="/erkaklar-uchun" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="person" />Erkaklar uchun</router-link>
+                </li>
+                <li>
+                    <router-link to="/ayollar-uchun" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="person-dress" />Ayollar uchun</router-link>
+                </li>
+                <li>
+                    <router-link to="/oziq-ovqat" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="apple-whole" />Oziq-ovqat mahsulotlari</router-link>
+                </li>
+                <li>
+                    <router-link to="/sport-va-hordiq" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="basketball" />Sport va hordiq </router-link>
+                </li>
+                <li>
+                    <router-link to="/mebellar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="home" />Uy jihozlari</router-link>
+                </li>
+                <li>
+                    <router-link to="/oshxona-jihozlari" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="kitchen-set" />Oshxona-jihozlari</router-link>
+                </li>
+                <li>
+                    <router-link to="/smartfonlar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="mobile" />Smartfonlar</router-link>
+                </li>
+            </ul>
+        </div>
     </nav>
-    <div :class="['nav__modal', { 'nav__modal-open': isModalOpen }]">
-        <ul class="nav__modal-list">
-            <li>
-                <router-link to="/avtomobillar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="car" />Avtomobillar </router-link>
-            </li>
-            <li>
-                <router-link to="/aksesuarlar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="headphones" />Aksessuarlar </router-link>
-            </li>
-            <li>
-                <router-link to="/planshetlar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="tablet" />Planshetlar</router-link>
-            </li>
-            <li>
-                <router-link to="/maishiy-kimyo-vositalari" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="pump-soap" />Teri parvarishi</router-link>
-            </li>
-            <li>
-                <router-link to="/mototransportlar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="motorcycle" />Mototransportlar</router-link>
-            </li>
-            <li>
-                <router-link to="/ko`zoynaklar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="glasses" />Ko'zoynaklar</router-link>
-            </li>
-            <li>
-                <router-link to="/noutbuklar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="laptop" />Noutbuklar</router-link>
-            </li>
-            <li>
-                <router-link to="/uy-dekaratsiyalari" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="chair" />Uy dekoratsiyalari</router-link>
-            </li>
-            <li>
-                <router-link to="/erkaklar-uchun" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="person" />Erkaklar uchun</router-link>
-            </li>
-            <li>
-                <router-link to="/ayollar-uchun" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="person-dress" />Ayollar uchun</router-link>
-            </li>
-            <li>
-                <router-link to="/oziq-ovqat" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="apple-whole" />Oziq-ovqat mahsulotlari</router-link>
-            </li>
-            <li>
-                <router-link to="/sport-va-hordiq" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="basketball" />Sport va hordiq </router-link>
-            </li>
-            <li>
-                <router-link to="/mebellar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="home" />Uy jihozlari</router-link>
-            </li>
-            <li>
-                <router-link to="/oshxona-jihozlari" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="kitchen-set" />Oshxona-jihozlari</router-link>
-            </li>
-            <li>
-                <router-link to="/smartfonlar" class="nav__modal-list-link" @click="toggleModal"> <font-awesome-icon icon="mobile" />Smartfonlar</router-link>
-            </li>
-        </ul>
-    </div>
 </template>
