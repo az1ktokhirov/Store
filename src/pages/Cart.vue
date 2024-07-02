83113519e5337e3272866c2aa36f37283147e2fc
az1ktokhirov <azizillotokhirov@gmail.com>
2024-07-02 14:57:22 +0500

    update

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -40,15 +40,7 @@
     <div class="nothing" v-else>
       <div class="container">
         <div class="nothing__content">
-          <svg width="150px" height="150px" viewBox="-3.12 -3.12 30.24 30.24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#2b2b2b" transform="rotate(0)">
-            <g id="SVGRepo_bgCarrier" stroke-width="0" transform="translate(3.3599999999999994,3.3599999999999994), scale(0.72)">
-              <path transform="translate(-3.12, -3.12), scale(1.8900000000000001)" fill="#ffe9d1" d="M9.166.33a2.25 2.25 0 00-2.332 0l-5.25 3.182A2.25 2.25 0 00.5 5.436v5.128a2.25 2.25 0 001.084 1.924l5.25 3.182a2.25 2.25 0 002.332 0l5.25-3.182a2.25 2.25 0 001.084-1.924V5.436a2.25 2.25 0 00-1.084-1.924L9.166.33z" strokewidth="0"></path>
-            </g>
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#3b3b3b3b" stroke-width="0.048"></g>
-            <g id="SVGRepo_iconCarrier">
-              <path d="M21 5L19 12H7.37671M20 16H8L6 3H3M11 3L13.5 5.5M13.5 5.5L16 8M13.5 5.5L16 3M13.5 5.5L11 8M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="#000000"></path>
-            </g>
-          </svg>
+          <img src="../assets/img/cart.png" alt="">
           <h6>Savatda hozircha mahsulot yoʻq</h6>
           <p>Bosh sahifadagi to’plamlardan boshlang yoki kerakli mahsulotni qidiruv orqali toping</p>
           <router-link to="/" class="nothing__content-link"><font-awesome-icon icon="home" /></router-link>                
